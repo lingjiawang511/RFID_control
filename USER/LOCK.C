fa07e24f9d5a683dcb36c9bcc8109134a9a41ba0
lingjiawang511 <398677675@qq.com>
2016-12-15 10:37:22 +0800

    add the project file

--- USER/LOCK.C
+++ USER/LOCK.C
@@ -2,16 +2,10 @@
 
 #define LOCK_SHORT_TIME 		20
 #define LOCK_LONG_TIME			300
-u8 Lock1_State;
-u8 Lock2_State;
-u8 Lock3_State;
-u8 Lock4_State;
-u16 lock1_time;
-u16 lock2_time;
-u16 lock3_time;
-u16 lock4_time;
+#define LOCK_STATE_COUNT    3			//锁检查滤波次数，在定时器调用就相当于滤波时间
 
 u8 Check_State;
+Lock_Type Lock;
 //=============================================================================
 //函数名称: LOCK_GPIO_Config
 //功能概要:LOCK灯引脚配置
@@ -102,9 +96,13 @@ void LOCK_GPIO_Config(void)
 	
 	
 	LOCK1_LIGHT_OFF();
-//	LOCK2_LIGHT_OFF();
 	LOCK1_OFF;
 	LOCK2_OFF;
+	Lock.lock1.lock_check_state = 0;
+	Lock.lock2.lock_check_state = 0;
+	Lock.lock3.lock_check_state = 0;
+	Lock.lock4.lock_check_state = 0;
+	
 }
 
 //锁的控制和检测分开，因为所的控制需要控制灯，并且人工关锁的时候要自动关灯，但是锁的状态改变需要上传给PC
@@ -115,7 +113,7 @@ static void Lock_control_12(void )
 	 static u8 lock2_triggerstate;
 	 static u16 lock2_timercount;
 	 //LOCK1
-	 if(Lock1_State == 1){	
+	 if(Lock.lock1.lock_state == 1){	
 			if(READ_LOCK1_CHECK == READLOW){   //锁是关的，才打开，不然没有意义
 				if(lock1_triggerstate == 0){
 					lock1_triggerstate = 1;
@@ -125,12 +123,12 @@ static void Lock_control_12(void )
 				if(lock1_timercount >= LOCK_SHORT_TIME){
 						LOCK1_LIGHT_ON_H();//检测到锁打开了，开灯并且关闭开锁信号,只有关闭了开锁信号，才可以人工关门
 						LOCK1_OFF;
-						Lock1_State = 2;
+						Lock.lock1.lock_state = 2;
 						lock1_triggerstate = 0;
 						lock1_timercount = 0;
 				}
 			}
-	 }else if(Lock1_State == 2){//开灯之后一直检查锁的状态，如果锁已经重新锁上，应该灭灯
+	 }else if(Lock.lock1.lock_state == 2){//开灯之后一直检查锁的状态，如果锁已经重新锁上，应该灭灯
 			if(READ_LOCK1_CHECK == READHIGH){
 				if(lock1_triggerstate == 0){
 					lock1_triggerstate = 1;
@@ -140,7 +138,7 @@ static void Lock_control_12(void )
 				if(lock1_timercount >= LOCK_SHORT_TIME){
 //						LOCK1_LIGHT_OFF();//关灯
 						LOCK1_OFF;
-						Lock1_State = 0;
+						Lock.lock1.lock_state = 0;
 						lock1_triggerstate = 0;
 						lock1_timercount = 0;
 				}
@@ -150,7 +148,7 @@ static void Lock_control_12(void )
 				lock1_timercount = 0;
 	 }
 	 	 //LOCK2
-	 if(Lock2_State == 1){
+	 if(Lock.lock2.lock_state == 1){
 			if(READ_LOCK2_CHECK == READLOW){
 				if(lock2_triggerstate == 0){
 					lock2_triggerstate = 1;
@@ -161,12 +159,12 @@ static void Lock_control_12(void )
 //						LOCK2_LIGHT_ON();//检测到锁打开了，开灯并且关闭开锁信号,只有关闭了开锁信号，才可以人工关门
 					  LOCK1_LIGHT_ON_H();//检测到锁打开了，开灯并且关闭开锁信号,只有关闭了开锁信号，才可以人工关门
 						LOCK2_OFF;
-						Lock2_State = 2;
+						Lock.lock2.lock_state = 2;
 						lock2_triggerstate = 0;
 						lock2_timercount = 0;
 				}
 			}
-	 }else if(Lock2_State == 2){//开灯之后一直检查锁的状态，如果锁已经重新锁上，应该灭灯
+	 }else if(Lock.lock2.lock_state == 2){//开灯之后一直检查锁的状态，如果锁已经重新锁上，应该灭灯
 			if(READ_LOCK2_CHECK == READHIGH){
 				if(lock2_triggerstate == 0){
 					lock2_triggerstate = 1;
@@ -177,7 +175,7 @@ static void Lock_control_12(void )
 //						LOCK2_LIGHT_OFF();//关灯
 //				  	LOCK1_LIGHT_OFF();//关灯
 						LOCK2_OFF;
-						Lock2_State = 0;
+						Lock.lock2.lock_state = 0;
 						lock2_triggerstate = 0;
 						lock2_timercount = 0;
 				}
@@ -187,18 +185,18 @@ static void Lock_control_12(void )
 				lock2_timercount = 0;
 	 }
 	 
-	 if(Lock1_State > 0){   //最长开锁时间，如果锁是开的，上位机让MCU开锁，超时后状态复位
-			 lock1_time--;
-			 if(lock1_time <= 0){
+	 if(Lock.lock1.lock_state > 0){   //最长开锁时间，如果锁是开的，上位机让MCU开锁，超时后状态复位
+			 Lock.lock1.lock_time--;
+			 if(Lock.lock1.lock_time <= 0){
 				 LOCK1_OFF;
-				 lock1_time = LOCK_TIME;
+				 Lock.lock1.lock_time = LOCK_TIME;
 			}
 	}
-	if(Lock2_State > 0){
-			 lock2_time--;
-			 if(lock2_time <= 0){
+	if(Lock.lock2.lock_state > 0){
+			 Lock.lock2.lock_time--;
+			 if(Lock.lock2.lock_time <= 0){
 				 LOCK2_OFF;
-				 lock2_time = LOCK_TIME;
+				 Lock.lock2.lock_time = LOCK_TIME;
 			}
 	}
 }
@@ -209,8 +207,8 @@ static void Lock_control_34(void )
 	 static u16 lock3_timercount;
 	 static u8 lock4_triggerstate;
 	 static u16 lock4_timercount;
-	 //LOCK1
-	 if(Lock3_State == 1){	
+	 //LOCK3
+	 if(Lock.lock3.lock_state == 1){	
 			if(READ_LOCK3_CHECK == READLOW){
 				if(lock3_triggerstate == 0){
 					lock3_triggerstate = 1;
@@ -220,12 +218,12 @@ static void Lock_control_34(void )
 				if(lock3_timercount >= LOCK_SHORT_TIME){
 						LOCK1_LIGHT_ON_H();//检测到锁打开了，开灯并且关闭开锁信号,只有关闭了开锁信号，才可以人工关门
 						LOCK3_OFF;
-						Lock3_State = 2;
+						Lock.lock3.lock_state = 2;
 						lock3_triggerstate = 0;
 						lock3_timercount = 0;
 				}
 			}
-	 }else if(Lock3_State == 2){//开灯之后一直检查锁的状态，如果锁已经重新锁上，应该灭灯
+	 }else if(Lock.lock3.lock_state == 2){//开灯之后一直检查锁的状态，如果锁已经重新锁上，应该灭灯
 			if(READ_LOCK3_CHECK == READHIGH){
 				if(lock3_triggerstate == 0){
 					lock3_triggerstate = 1;
@@ -235,7 +233,7 @@ static void Lock_control_34(void )
 				if(lock3_timercount >= LOCK_SHORT_TIME){
 //						LOCK1_LIGHT_OFF();//关灯
 						LOCK3_OFF;
-						Lock3_State = 0;
+						Lock.lock3.lock_state = 0;
 						lock3_triggerstate = 0;
 						lock3_timercount = 0;
 				}
@@ -245,7 +243,7 @@ static void Lock_control_34(void )
 				lock3_timercount = 0;
 	 }
 	 	 //LOCK4
-	 if(Lock4_State == 1){
+	 if(Lock.lock4.lock_state == 1){
 			if(READ_LOCK4_CHECK == READLOW){
 				if(lock4_triggerstate == 0){
 					lock4_triggerstate = 1;
@@ -256,12 +254,12 @@ static void Lock_control_34(void )
 //						LOCK2_LIGHT_ON();//检测到锁打开了，开灯并且关闭开锁信号,只有关闭了开锁信号，才可以人工关门
 					  LOCK1_LIGHT_ON_H();//检测到锁打开了，开灯并且关闭开锁信号,只有关闭了开锁信号，才可以人工关门
 						LOCK4_OFF;
-						Lock4_State = 2;
+						Lock.lock4.lock_state = 2;
 						lock4_triggerstate = 0;
 						lock4_timercount = 0;
 				}
 			}
-	 }else if(Lock4_State == 2){//开灯之后一直检查锁的状态，如果锁已经重新锁上，应该灭灯
+	 }else if(Lock.lock4.lock_state == 2){//开灯之后一直检查锁的状态，如果锁已经重新锁上，应该灭灯
 			if(READ_LOCK4_CHECK == READHIGH){
 				if(lock4_triggerstate == 0){
 					lock4_triggerstate = 1;
@@ -272,7 +270,7 @@ static void Lock_control_34(void )
 //						LOCK2_LIGHT_OFF();//关灯
 //				  	LOCK1_LIGHT_OFF();//关灯
 						LOCK4_OFF;
-						Lock4_State = 0;
+						Lock.lock4.lock_state = 0;
 						lock4_triggerstate = 0;
 						lock4_timercount = 0;
 				}
@@ -282,32 +280,97 @@ static void Lock_control_34(void )
 				lock4_timercount = 0;
 	 }
 	 
-	 if(Lock3_State > 0){
-			 lock3_time--;
-			 if(lock3_time <= 0){
+	 if(Lock.lock3.lock_state > 0){
+			 Lock.lock3.lock_time--;
+			 if(Lock.lock3.lock_time <= 0){
 				 LOCK3_OFF;
-				 lock3_time = LOCK_TIME;
+				 Lock.lock3.lock_time = LOCK_TIME;
 			}
 	}
-	if(Lock4_State > 0){
-			 lock4_time--;
-			 if(lock4_time <= 0){
+	if(Lock.lock4.lock_state > 0){
+			 Lock.lock4.lock_time--;
+			 if(Lock.lock4.lock_time <= 0){
 				 LOCK4_OFF;
-				 lock4_time = LOCK_TIME;
+				 Lock.lock4.lock_time = LOCK_TIME;
 			}
 	}
 }
 
+static void Lock_check(void )
+{
+	static u8 old_lock1_state,old_lock2_state,old_lock3_state,old_lock4_state;
+  static u8 lock1_state_count,lock2_state_count,lock3_state_count,lock4_state_count;	
+	
+	if(Lock.lock1.lock_check_state == 0){
+		if(old_lock1_state != READ_LOCK1_CHECK){
+				lock1_state_count++;
+				if(lock1_state_count >= LOCK_STATE_COUNT)
+				{
+					Lock.lock1.lock_check_state = 1;		//锁状态变化位，为1的时候需要主动上传数据，数据上传完之后清零
+					Lock.lock1.lock_check_value = READ_LOCK1_CHECK;
+					lock1_state_count = 0;
+					old_lock1_state = Lock.lock1.lock_check_value;
+				}
+			}else{
+					lock1_state_count = 0;
+			}
+	}
+	if(Lock.lock2.lock_check_state == 0){
+		if(old_lock2_state != READ_LOCK2_CHECK){
+				lock2_state_count++;
+				if(lock2_state_count >= LOCK_STATE_COUNT)
+				{
+					Lock.lock2.lock_check_state = 1;
+					Lock.lock2.lock_check_value = READ_LOCK2_CHECK;
+					lock2_state_count = 0;
+					old_lock2_state = Lock.lock2.lock_check_value;
+				}
+			}else{
+					lock2_state_count = 0;
+			}
+	 }
+	if(Lock.lock3.lock_check_state == 0){
+			if(old_lock3_state != READ_LOCK3_CHECK){
+				lock3_state_count++;
+				if(lock3_state_count >= LOCK_STATE_COUNT)
+				{
+					Lock.lock3.lock_check_state = 1;
+					Lock.lock3.lock_check_value = READ_LOCK3_CHECK;
+					lock3_state_count = 0;
+					old_lock3_state = Lock.lock3.lock_check_value;
+				}
+			}else{
+					lock3_state_count = 0;
+			}
+	 }
+	 if(Lock.lock4.lock_check_state == 0){
+			if(old_lock4_state != READ_LOCK4_CHECK){
+				lock4_state_count++;
+				if(lock4_state_count >= LOCK_STATE_COUNT)
+				{
+					Lock.lock4.lock_check_state = 1;
+					Lock.lock4.lock_check_value = READ_LOCK4_CHECK;
+					lock4_state_count = 0;
+					old_lock4_state = Lock.lock4.lock_check_value;
+					lock4_state_count = 0;
+				}
+			}else{
+					lock4_state_count = 0;
+			}
+	}	
+}
 //必须在定时器中调用此函数
 void Lock_control(void )
 {
 	Lock_control_12();
 	Lock_control_34();
-	if((Lock1_State > 0)||(Lock2_State > 0)||(Lock3_State > 0)||(Lock4_State > 0)){
+	if((Lock.lock1.lock_state > 0)||(Lock.lock2.lock_state > 0)||(Lock.lock3.lock_state > 0)||(Lock.lock4.lock_state > 0)){
 		 LOCK1_LIGHT_ON_H();//检测到锁打开了，开灯并且关闭开锁信号,只有关闭了开锁信号，才可以人工关门
 	}else{//四个锁都是关的状态，自动关灯
 		LOCK1_LIGHT_OFF();//关灯
 	}
+	
+	Lock_check();
 }
 
 
